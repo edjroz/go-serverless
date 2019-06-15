a8f9f70aca098f365ea1fdc49ab8af16b785853b
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-06-15 09:53:28 -0400

    add CORS

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
   var data struct {
     Message string `json:"message"`
   }
+
+  cors := checkCORS(w, r); if cors {
+    return
+  }
+
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+
   if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
     fmt.Fprint(w, "Hello World!")
     return
@@ -20,4 +27,17 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
     return
   }
   fmt.Fprint(w, html.EscapeString(data.Message))
+}
+
+func checkCORS(w http.ResponseWriter, r *http.Request) (ok bool) {
+  ok = false
+  if r.Method == http.MethodOptions {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+    w.Header().Set("Access-Control-Max-Age", "3600")
+    w.WriteHeader(http.StatusNoContent)
+    ok = true
+  }
+  return
 }
