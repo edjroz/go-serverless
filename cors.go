44f396b3478b436f42869230ebc6839f5bd31349
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-06-18 15:52:20 -0400

    move tool individual file

---
+++ cors.go
@@ -0,0 +1,16 @@
+package serverless
+
+import "net/http"
+
+func checkCORS(w http.ResponseWriter, r *http.Request) (ok bool) {
+  ok = false
+  if r.Method == http.MethodOptions {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+    w.Header().Set("Access-Control-Max-Age", "3600")
+    w.WriteHeader(http.StatusNoContent)
+    ok = true
+  }
+  return
+}
--- main.go
+++ main.go
@@ -28,16 +28,3 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
   }
   fmt.Fprint(w, html.EscapeString(data.Message))
 }
-
-func checkCORS(w http.ResponseWriter, r *http.Request) (ok bool) {
-  ok = false
-  if r.Method == http.MethodOptions {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "POST")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-    w.Header().Set("Access-Control-Max-Age", "3600")
-    w.WriteHeader(http.StatusNoContent)
-    ok = true
-  }
-  return
-}
