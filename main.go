19b39255ab9d801ac2ca3ba7f78b7141d339a6b8
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-06-21 17:35:01 -0400

    remove json content

--- main.go
+++ main.go
@@ -30,23 +30,7 @@ func init() {
 }
 func SignImage(w http.ResponseWriter, r *http.Request) {
   // TODO: how should your data be??
-  var data struct {
-    Message string `json:"message"`
-  }
-
   cors := checkCORS(w, r); if cors {
     return
   }
-
-  w.Header().Set("Access-Control-Allow-Origin", "*")
-
-  if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-    fmt.Fprint(w, "Hello World!")
-    return
-  }
-  if data.Message == "" {
-    fmt.Fprint(w, "Hello World!")
-    return
-  }
-  fmt.Fprint(w, html.EscapeString(data.Message))
 }
