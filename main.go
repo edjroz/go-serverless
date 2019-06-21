a525d26acdc7c2618af8d060c793f690d4446533
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-06-21 17:28:25 -0400

    add lazy loading to fetching image

--- main.go
+++ main.go
@@ -5,9 +5,31 @@ import (
   "fmt"
   "html"
   "net/http"
+  "imageUri"
+  "io"
 )
 
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
+var (
+  imageUri string
+  image io.ReadCloser
+)
+func init() {
+  if baseUri == nil {
+    imageUri = os.GetEnv("BASE_IMAGE_URI")
+  }
+  if image == nil {
+    var err error
+    imagePayload, err := http.Get(baseUri)
+    if err != nil {
+      fmt.Fprintf(w, "Error %d", err)
+      return
+    }
+    defer imagePayload.Body.Close()
+    image = imagePayload.Body
+  }
+}
+func SignImage(w http.ResponseWriter, r *http.Request) {
+  // TODO: how should your data be??
   var data struct {
     Message string `json:"message"`
   }
